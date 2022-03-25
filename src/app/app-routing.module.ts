e5927410925fcb966d8add41d333fe47b1e3a1d4
bookman <nassim.t998@gmail.com>
2022-03-25 11:52:35 +0100

    Création du projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {ConnexionComponent} from './connexion/connexion.component';
+import {InscriptionComponent} from './inscription/inscription.component';
+import {NotfoundComponent} from './notfound/notfound.component';
+
+
+const routes: Routes = [
+  { path: 'inscription-component', component: InscriptionComponent },
+  { path: 'connexion-component', component: ConnexionComponent },
+  { path: 'inscriptionX',   redirectTo: '/inscription-component', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {InscriptionComponent} from './inscription/inscription.component';
+import {ConnexionComponent} from './connexion/connexion.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    ConnexionComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
