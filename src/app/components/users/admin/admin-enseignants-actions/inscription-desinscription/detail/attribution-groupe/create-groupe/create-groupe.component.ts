541079c78fbb00383983bbbab3e5e29d744ed127
bookman <nassim.t998@gmail.com>
2022-05-26 23:59:12 +0200

    [Admin-Enseignant] Création et attribution d'un groupe d'étudiants à un enseignant

--- src/app/components/users/admin/admin-enseignants-actions/inscription-desinscription/detail/attribution-groupe/create-groupe/create-groupe.component.ts
+++ src/app/components/users/admin/admin-enseignants-actions/inscription-desinscription/detail/attribution-groupe/create-groupe/create-groupe.component.ts
@@ -1,5 +1,10 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { AdminService } from 'src/app/services/users/admin/admin.service';
+import { FormGroup, FormControl, Validators, AnyForUntypedForms } from '@angular/forms';
 import { FormationsService } from './../../../../../../../../services/formations/formations.service';
 import { Component, OnInit } from '@angular/core';
+import { AnimationDurations, MatOption } from '@angular/material/core';
 
 @Component({
   selector: 'app-create-groupe',
@@ -8,16 +13,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateGroupeComponent implements OnInit {
 
-
   modules : any[] = [];
+  nomGroupeForm = new FormGroup({nom : new FormControl('' , [Validators.required])})
+  enseignantID : any;
+  typesGroupe: any[] =  ["CM" , "TD" , "TP"]
 
-  constructor(private formationService : FormationsService) { }
+
+
+  constructor(
+    private formationService : FormationsService,
+    private adminService : AdminService,
+    private activeRoute : ActivatedRoute,
+    private snackBar : MatSnackBar) {}
 
   ngOnInit() {
       this.formationService.getAllFormations().subscribe((data) => {
              this.modules = data as [];
              console.log(this.modules)
+
       });
+      this.enseignantID = this.activeRoute.snapshot.paramMap.get("id");
+      console.log(this.enseignantID);
+  }
+
+  createGroupeEtudiant(selected : any , type: string){
+       if(this.nomGroupeForm.valid){
+
+          const nomGroupe = this.nomGroupeForm.value.nom
+          const idEnseignement = selected.value["_id"]
+
+          this.adminService.attributionGroupeEnseignant({
+            nomGroupe: nomGroupe,
+            typeGroupe: type,
+            nombreEtudiants: 0,
+            idEnseignant: this.enseignantID,
+            idEnseignement: idEnseignement,}).subscribe((data) => {
+                  if(data.success){
+                        this.snackBar.open("Groupe "+nomGroupe+" ajouté à l'enseignant" , "Fermer" ,  {duration: 2000})
+                  }
+                  else{
+                    this.snackBar.open(data.message, "Fermer")
+                  }
+            })
+
+            this.nomGroupeForm.reset()
+       }
   }
 
 }
